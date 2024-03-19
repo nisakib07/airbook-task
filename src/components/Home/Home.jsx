c41c218f9e10aa8368cff66f7d1aa8c941f1f74c
nisakib07 <websakib07@gmail.com>
2024-03-19 19:42:54 +0600

    Navbar done

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,16 +1,20 @@
 import AboutUs from "../AboutUs/AboutUs";
 import Airlines from "../Airlines/Airlines";
 import Banner from "../Banner/Banner";
+import Blog from "../Blog/Blog";
 import Deals from "../Deals/Deals";
 import Featured from "../Featured/Featured";
+import Navbar from "../Navbar/Navbar";
 
 const Home = () => {
   return (
     <div>
+      <Navbar></Navbar>
       <Banner></Banner>
       <Featured></Featured>
       <Deals></Deals>
       <Airlines></Airlines>
+      <Blog></Blog>
       {/* <AboutUs></AboutUs> */}
     </div>
   );
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,18 +1,32 @@
+import logo from "../../assets/logo.png";
 const Navbar = () => {
   return (
-    <nav className="bg-gray-200 p-4">
-      <div className="flex justify-between">
-        <div className="text-2xl font-bold">
-          <span className="text-blue-500"></span> AirBook
-        </div>
-        <div className="flex space-x-2">
-          <div>Blog</div>
-          <div>Offer</div>
-          <div>Airlines</div>
-          <div>About</div>
+    <div className="w-full fixed">
+      <div className=" bg-white">
+        <div className="flex justify-between items-center py-5 w-[1280px] mx-auto">
+          <div className="flex items-center gap-2">
+            <img className="w-6" src={logo} alt="" />
+            <h1 className="text-3xl">
+              <span className="text-yellow-500 font-bold">Air</span>
+              <span className="text-blue-500">Book</span>
+            </h1>
+          </div>
+          <div>
+            <ul className="flex space-x-8 items-center text-lg font-medium text-textPrimary">
+              <li>Blog</li>
+              <li>Offer</li>
+              <li>Airlines</li>
+              <li>About</li>
+            </ul>
+          </div>
+          <div>
+            <button className="bg-[#006CE4] text-white px-8 text-base font-medium py-2 rounded-[10px]">
+              Sign in
+            </button>
+          </div>
         </div>
       </div>
-    </nav>
+    </div>
   );
 };
 
